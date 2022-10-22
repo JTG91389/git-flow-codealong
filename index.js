4cc0f5e784b916f84d33fa03a53a0bbef1ea2927
Joel <jgear91389@gmail.com>
2022-10-22 12:53:31 -0500

    adding user routes (#2)

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+
 console.log('testing')
+
+const express = require('express');
+const app = express();
+const routes = require("./routes/index");
+
+app.use('/user', routes)
+
+app.use((req, res, next) => {
+    res.status(404).send(
+        "<h1>Page not found on the server</h1>")
+})
+
+app.listen(8000);
+
